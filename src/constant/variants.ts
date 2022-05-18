76b8387069ac8da6612dc148c8ee4600d8b13b7a
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-18 16:35:30 +0900

    ADD: Startpage에 대한 variants 추가

--- src/constant/variants.ts
+++ src/constant/variants.ts
@@ -1,3 +1,30 @@
+export const swirlFadeOut = {
+  initial: {
+    scale: 0,
+  },
+  animate: {
+    scale: [0, 0.5, 1, 1.25, 0.85, 1],
+    transition: {
+      type: "tween",
+      duration: 0.3,
+      staggerChildren: 1,
+    },
+  },
+
+  exit: {
+    originX: 0.5,
+    originY: 1.5,
+    scale: [1.25, 0.85, 1, 0.75, 0.5, 0.25, 0],
+    rotate: [0, -60, -120, -180, -240, -300, -360],
+    transition: {
+      type: "linear",
+      ease: "easeInOut",
+      duration: 0.5,
+      when: "afterChildren",
+    },
+  },
+};
+
 export const popUp = {
   initial: {
     scale: 0,
