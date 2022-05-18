0f574b04a04b57e859f04c463f1df242838398e9
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-18 16:39:48 +0900

    MODIFY:BoardType추가 ADD: createNextBoard 함수 추가 MODIFY: setState로직을 changeTurn 내부로 이적 MODIFY: 변경된 checkGameEnd 함수를 import 하여 적용

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,9 +3,9 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import Cell from "../Cell";
 
-import { checkGameEnd } from "../../utils/checkGameEnd";
+import { checkGameEnd } from "../../utils/checkGameSet";
 import { popUp } from "../../constant/variants";
-import { Turn, Winner } from "../../Types";
+import { BoardType, Turn, Winner } from "../../Types";
 
 interface Props {
   turn: Turn;
@@ -16,40 +16,38 @@ interface Props {
 const SIDE = 9;
 
 export const Board = ({ turn, setTurn, showResult }: Props) => {
-  const [board, setBoard] = useState<string[][]>([]);
+  const [board, setBoard] = useState<BoardType>([]);
   const [leftCells, setLeftCells] = useState<number>(SIDE ** 2);
 
-  const clickCell = (row: number, col: number) => {
-    // 이미 점령되어 있는 칸이라면 pass
-    if (board[row][col] !== "") return;
-
-    const newBoard = [...board];
-    updateBoard(newBoard, row, col);
-
-    const isGameEnd = checkGameEnd({ newBoard, row, col, flag: turn });
-    if (isGameEnd) showResult(turn);
-
-    if (leftCells === 1) showResult("DRAW");
-    changeTurn();
-  };
-
-  const changeTurn = () => {
+  const changeTurn = (newBoard: BoardType) => {
     setTurn((prevTurn) => (prevTurn === "X" ? "O" : "X"));
+    setBoard(newBoard);
     setLeftCells((prevCells) => prevCells - 1);
   };
 
-  const updateBoard = (newBoard: string[][], row: number, col: number) => {
+  const createNextBoard = (row: number, col: number) => {
+    const newBoard = [...board];
     newBoard[row][col] = turn;
-    setBoard(newBoard);
+    return newBoard;
+  };
+
+  const handleCellClick = (row: number, col: number) => {
+    // 이미 점령되어 있는 칸이라면 pass
+    if (board[row][col] !== "") return;
+
+    const newBoard = createNextBoard(row, col);
+
+    const isGameEnd = checkGameEnd({ board: newBoard, row, col, flag: turn });
+    if (isGameEnd) showResult(turn);
+    else if (leftCells === 1) showResult("DRAW");
+    else changeTurn(newBoard);
   };
 
   useEffect(() => {
+    const generateBoard = () =>
+      Array.from({ length: SIDE }, () => Array(SIDE).fill(""));
+
     setBoard(generateBoard());
-
-    function generateBoard() {
-      return Array.from({ length: SIDE }, () => Array(SIDE).fill(""));
-    }
-
     return () => setBoard([]);
   }, []);
 
@@ -62,7 +60,7 @@ export const Board = ({ turn, setTurn, showResult }: Props) => {
               key={colIdx}
               turn={turn}
               isOccupied={board[rowIdx][colIdx] !== ""}
-              onClick={() => clickCell(rowIdx, colIdx)}
+              onClick={() => handleCellClick(rowIdx, colIdx)}
             >
               <Cell flag={board[rowIdx][colIdx]} />
             </CellWrapper>
